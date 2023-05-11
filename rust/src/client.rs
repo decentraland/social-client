571f5c8bebc87541093c0269948fe5d2d249368d
Juli Milloch <millochjuli@gmail.com>
2023-05-11 15:33:11 +0200

    review: refactor by extracting the common logic

--- rust/src/client.rs
+++ rust/src/client.rs
@@ -43,126 +43,143 @@ impl Flow {
         match self {
             Flow::Flow1 => {
                 // Implement Flow 1: Request A-B, Cancel A-B
-                request_a_to_b(module, token_user_a, user_b_address).await;
-                cancel_a_to_b(module, token_user_a, user_b_address).await;
+                request(module, token_user_a, user_b_address).await;
+                cancel(module, token_user_a, user_b_address).await;
             }
             Flow::Flow2 => {
                 // Implement Flow 2: Request A-B, Accept B-A, Delete A-B
-                request_a_to_b(module, token_user_a, user_b_address).await;
-                accept_b_to_a(module, token_user_b, user_a_address).await;
-                delete_a_to_b(module, token_user_a, user_b_address).await;
+                request(module, token_user_a, user_b_address).await;
+                accept(module, token_user_b, user_a_address).await;
+                delete(module, token_user_a, user_b_address).await;
             }
             Flow::Flow3 => {
                 // Implement Flow 3: Request A-B, Reject B-A
-                request_a_to_b(module, token_user_a, user_b_address).await;
-                reject_b_to_a(module, token_user_b, user_a_address).await;
+                request(module, token_user_a, user_b_address).await;
+                reject(module, token_user_b, user_a_address).await;
             }
             Flow::Flow4 => {
                 // Implement Flow 4: Request A-B, Accept B-A, Delete B-A
-                request_a_to_b(module, token_user_a, user_b_address).await;
-                accept_b_to_a(module, token_user_b, user_a_address).await;
-                delete_b_to_a(module, token_user_a, user_b_address).await;
+                request(module, token_user_a, user_b_address).await;
+                accept(module, token_user_b, user_a_address).await;
+                delete(module, token_user_a, user_b_address).await;
             }
         }
     }
 }
 
-async fn request_a_to_b(
+async fn request(
     module: &FriendshipsServiceClient<WebSocketTransport>,
-    token_user_a: &str,
-    user_b_address: &str,
+    token: &str,
+    user_address: &str,
 ) {
     let request_payload = RequestPayload {
         user: Some(User {
-            address: user_b_address.to_string(),
+            address: user_address.to_string(),
         }),
-        message: Some("A message from userA to userB".to_string()),
+        message: Some("A message".to_string()),
     };
-    let request_event = FriendshipEventPayload {
-        body: Some(friendship_event_payload::Body::Request(request_payload)),
-    };
-    let request_a_to_b_response = module
-        .update_friendship_event(UpdateFriendshipPayload {
-            event: Some(request_event),
-            auth_token: Some(Payload {
-                synapse_token: Some(token_user_a.to_string()),
-            }),
-        })
-        .await;
-    match request_a_to_b_response {
-        Ok(request_a_to_b_response) => {
-            println!(
-                "> Server Unary > Response > UpdateFrienshipResponse Request::A->B {request_a_to_b_response:?}"
-            );
-        }
-        Err(err) => {
-            panic!("{err:?}")
-        }
-    }
 
-    // The state resolution from synapse takes some time
-    sleep(Duration::from_secs(5)).await;
+    update_friendship_event(
+        module,
+        token,
+        friendship_event_payload::Body::Request(request_payload),
+    )
+    .await;
 }
 
-async fn cancel_a_to_b(
+async fn cancel(
     module: &FriendshipsServiceClient<WebSocketTransport>,
-    token_user_a: &str,
-    user_b_address: &str,
+    token: &str,
+    user_address: &str,
 ) {
     let cancel_payload = CancelPayload {
         user: Some(User {
-            address: user_b_address.to_string(),
+            address: user_address.to_string(),
         }),
     };
-    let cancel_event = FriendshipEventPayload {
-        body: Some(friendship_event_payload::Body::Cancel(cancel_payload)),
-    };
-    let a = module
-        .update_friendship_event(UpdateFriendshipPayload {
-            event: Some(cancel_event),
-            auth_token: Some(Payload {
-                synapse_token: Some(token_user_a.to_string()),
-            }),
-        })
-        .await;
-    match a {
-        Ok(cancel_a_to_b_response) => {
-            println!(
-                "> Server Unary > Response > UpdateFrienshipResponse Cancel::A->B {cancel_a_to_b_response:?}"
-            );
-        }
-        Err(err) => {
-            panic!("{err:?}")
-        }
-    }
+
+    update_friendship_event(
+        module,
+        token,
+        friendship_event_payload::Body::Cancel(cancel_payload),
+    )
+    .await;
 }
 
-async fn accept_b_to_a(
+async fn accept(
     module: &FriendshipsServiceClient<WebSocketTransport>,
-    token_user_b: &str,
-    user_a_address: &str,
+    token: &str,
+    user_address: &str,
 ) {
     let accept_payload = AcceptPayload {
         user: Some(User {
-            address: user_a_address.to_string(),
+            address: user_address.to_string(),
         }),
     };
-    let accept_event = FriendshipEventPayload {
-        body: Some(friendship_event_payload::Body::Accept(accept_payload)),
+
+    update_friendship_event(
+        module,
+        token,
+        friendship_event_payload::Body::Accept(accept_payload),
+    )
+    .await;
+}
+
+async fn reject(
+    module: &FriendshipsServiceClient<WebSocketTransport>,
+    token: &str,
+    user_address: &str,
+) {
+    let reject_payload = RejectPayload {
+        user: Some(User {
+            address: user_address.to_string(),
+        }),
     };
-    let accept_b_to_a_response = module
+
+    update_friendship_event(
+        module,
+        token,
+        friendship_event_payload::Body::Reject(reject_payload),
+    )
+    .await;
+}
+
+async fn delete(
+    module: &FriendshipsServiceClient<WebSocketTransport>,
+    token: &str,
+    user_address: &str,
+) {
+    let delete_payload = DeletePayload {
+        user: Some(User {
+            address: user_address.to_string(),
+        }),
+    };
+
+    update_friendship_event(
+        module,
+        token,
+        friendship_event_payload::Body::Delete(delete_payload),
+    )
+    .await;
+}
+
+async fn update_friendship_event(
+    module: &FriendshipsServiceClient<WebSocketTransport>,
+    token: &str,
+    body: friendship_event_payload::Body,
+) {
+    let event_payload = FriendshipEventPayload { body: Some(body) };
+    let response = module
         .update_friendship_event(UpdateFriendshipPayload {
-            event: Some(accept_event),
+            event: Some(event_payload),
             auth_token: Some(Payload {
-                synapse_token: Some(token_user_b.to_string()),
+                synapse_token: Some(token.to_string()),
             }),
         })
         .await;
-    match accept_b_to_a_response {
-        Ok(accept_b_to_a_response) => {
-            println!(
-                "> Server Unary > Response > UpdateFrienshipResponse Accept::B->A {accept_b_to_a_response:?}"
-            );
+    match response {
+        Ok(response) => {
+            println!("> Server Unary > Response > UpdateFrienshipResponse:: {response:?}");
         }
         Err(err) => {
             panic!("{err:?}")
@@ -171,105 +188,6 @@ async fn accept_b_to_a(
 
     // The state resolution from synapse takes some time
     sleep(Duration::from_secs(5)).await;
-}
-
-async fn reject_b_to_a(
-    module: &FriendshipsServiceClient<WebSocketTransport>,
-    token_user_b: &str,
-    user_a_address: &str,
-) {
-    let reject_payload = RejectPayload {
-        user: Some(User {
-            address: user_a_address.to_string(),
-        }),
-    };
-    let reject_event = FriendshipEventPayload {
-        body: Some(friendship_event_payload::Body::Reject(reject_payload)),
-    };
-    let reject_b_to_a_response = module
-        .update_friendship_event(UpdateFriendshipPayload {
-            event: Some(reject_event),
-            auth_token: Some(Payload {
-                synapse_token: Some(token_user_b.to_string()),
-            }),
-        })
-        .await;
-    match reject_b_to_a_response {
-        Ok(reject_b_to_a_response) => {
-            println!(
-              "> Server Unary > Response > UpdateFrienshipResponse Reject::B->A {reject_b_to_a_response:?}"
-          );
-        }
-        Err(err) => {
-            panic!("{err:?}")
-        }
-    }
-}
-
-async fn delete_a_to_b(
-    module: &FriendshipsServiceClient<WebSocketTransport>,
-    token_user_a: &str,
-    user_b_address: &str,
-) {
-    let delete_payload = DeletePayload {
-        user: Some(User {
-            address: user_b_address.to_string(),
-        }),
-    };
-    let delete_event = FriendshipEventPayload {
-        body: Some(friendship_event_payload::Body::Delete(delete_payload)),
-    };
-    let delete_a_to_b_response = module
-        .update_friendship_event(UpdateFriendshipPayload {
-            event: Some(delete_event),
-            auth_token: Some(Payload {
-                synapse_token: Some(token_user_a.to_string()),
-            }),
-        })
-        .await;
-    match delete_a_to_b_response {
-        Ok(delete_a_to_b_response) => {
-            println!(
-                "> Server Unary > Response > UpdateFrienshipResponse Delete::A->B {delete_a_to_b_response:?}"
-            );
-        }
-        Err(err) => {
-            panic!("{err:?}")
-        }
-    }
-}
-
-async fn delete_b_to_a(
-    module: &FriendshipsServiceClient<WebSocketTransport>,
-    token_user_b: &str,
-    user_a_address: &str,
-) {
-    let delete_payload = DeletePayload {
-        user: Some(User {
-            address: user_a_address.to_string(),
-        }),
-    };
-    let delete_event = FriendshipEventPayload {
-        body: Some(friendship_event_payload::Body::Delete(delete_payload)),
-    };
-    let delete_b_to_a_response = module
-        .update_friendship_event(UpdateFriendshipPayload {
-            event: Some(delete_event),
-            auth_token: Some(Payload {
-                synapse_token: Some(token_user_b.to_string()),
-            }),
-        })
-        .await;
-    match delete_b_to_a_response {
-        Ok(delete_b_to_a_response) => {
-            println!(
-              "> Server Unary > Response > UpdateFrienshipResponse Delete::B->A {delete_b_to_a_response:?}"
-          );
-        }
-        Err(err) => {
-            panic!("{err:?}")
-        }
-    }
 }
 
 #[tokio::main]
