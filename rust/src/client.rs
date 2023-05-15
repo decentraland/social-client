884a0960f3f6428559ac5828f5b7b7a542588019
Agustina Aldasoro <agusaldasoro@users.noreply.github.com>
2023-05-15 12:49:52 -0400

    Add credentials file (#5)

--- rust/src/client.rs
+++ rust/src/client.rs
@@ -1,6 +1,5 @@
-use std::{env, time::Duration};
 
-use std::io::{self, Write};
+use std::{env, time::Duration, io::{self, Write}};
 
 use dcl_rpc::{
     client::RpcClient,
@@ -37,33 +36,31 @@ impl Flow {
     async fn execute(
         &self,
         module: &FriendshipsServiceClient<WebSocketTransport>,
-        token_user_a: &str,
-        token_user_b: &str,
-        user_a_address: &str,
-        user_b_address: &str,
+        user_a: AuthUser,
+        user_b: AuthUser,
     ) {
         match self {
             Flow::Flow1 => {
                 // Implement Flow 1: Request A-B, Cancel A-B
-                request(module, token_user_a, user_b_address).await;
-                cancel(module, token_user_a, user_b_address).await;
+                request(module, &user_a.token, &user_b.address).await;
+                cancel(module, &user_a.token, &user_b.address).await;
             }
             Flow::Flow2 => {
                 // Implement Flow 2: Request A-B, Accept B-A, Delete A-B
-                request(module, token_user_a, user_b_address).await;
-                accept(module, token_user_b, user_a_address).await;
-                delete(module, token_user_a, user_b_address).await;
+                request(module, &user_a.token, &user_b.address).await;
+                accept(module, &user_b.token, &user_a.address).await;
+                delete(module, &user_a.token, &user_b.address).await;
             }
             Flow::Flow3 => {
                 // Implement Flow 3: Request A-B, Reject B-A
-                request(module, token_user_a, user_b_address).await;
-                reject(module, token_user_b, user_a_address).await;
+                request(module, &user_a.token, &user_b.address).await;
+                reject(module, &user_b.token, &user_a.address).await;
             }
             Flow::Flow4 => {
                 // Implement Flow 4: Request A-B, Accept B-A, Delete B-A
-                request(module, token_user_a, user_b_address).await;
-                accept(module, token_user_b, user_a_address).await;
-                delete(module, token_user_a, user_b_address).await;
+                request(module, &user_a.token, &user_b.address).await;
+                accept(module, &user_b.token, &user_a.address).await;
+                delete(module, &user_a.token, &user_b.address).await;
             }
         }
     }
@@ -202,21 +199,28 @@ async fn get_input(prompt: &str) -> io::Result<String> {
     Ok(buffer.trim_end().to_owned())
 }
 
-async fn get_inputs() -> (String, String, String, String) {
-    let token_user_a = get_input("Enter Token for User A: ").await.unwrap();
-    let token_user_b = get_input("Enter Token for User B: ").await.unwrap();
-    let user_a_address = get_input("Enter Address for User A: ").await.unwrap();
-    let user_b_address = get_input("Enter Address for User B: ").await.unwrap();
 
-    (token_user_a, token_user_b, user_a_address, user_b_address)
+
+#[derive(Clone, Debug)]
+struct AuthUser {
+    address: String,
+    token: String,
 }
 
 #[tokio::main]
 async fn main() {
-    let (token_user_a, token_user_b, user_a_address, user_b_address) = get_inputs().await;
+    // Get the flow to execute from command-line arguments
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        panic!("Please provide a flow to execute");
+    }
+    let flow = Flow::from_str(&args[1]).expect("Invalid flow");
 
-    // let host = "wss://rpc-social-service.decentraland.zone";
-    let host = "ws://127.0.0.1:8085";
+    // Auth Users
+    let (user_a, user_b) = load_users().await;
+
+    let host = "wss://rpc-social-service.decentraland.zone";
+    // let host = "ws://127.0.0.1:8085";
 
     let client_connection = WebSocketClient::connect(host).await.unwrap();
 
@@ -234,7 +238,7 @@ async fn main() {
     // 1. Get Friends message
     let friends_response = module
         .get_friends(Payload {
-            synapse_token: Some(token_user_a.to_string()),
+            synapse_token: Some(user_a.clone().token),
         })
         .await;
     match friends_response {
@@ -254,7 +258,7 @@ async fn main() {
     // 2. Get Requests Events message
     let friendship_request_events = module
         .get_request_events(Payload {
-            synapse_token: Some(token_user_a.to_string()),
+            synapse_token: Some(user_a.clone().token),
         })
         .await;
     match friendship_request_events {
@@ -269,19 +273,53 @@ async fn main() {
     }
 
     // 3. Update Friendship Events message
-    // Get the flow to execute from command-line arguments
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        panic!("Please provide a flow to execute");
-    }
-    let flow = Flow::from_str(&args[1]).expect("Invalid flow");
-
     flow.execute(
         &module,
-        &token_user_a,
-        &token_user_b,
-        &user_a_address,
-        &user_b_address,
+        user_a,
+        user_b,
     )
     .await;
+}
+
+
+async fn load_users() -> (AuthUser, AuthUser) {
+    // Read token from file
+    match std::fs::read_to_string("credentials.json") {
+        Ok(it) => {
+            let users = serde_json::from_str::<serde_json::Value>(&it).unwrap();
+            let users = &users["users"];
+            
+            let user_a = extract_user(&users[0], "A").await;
+            let user_b = extract_user(&users[1], "B").await;
+
+            (user_a, user_b)
+        },
+        Err(_) => {
+            // If missing read from stdin
+            let token_user_a = get_input("Enter Token for User A: ").await.unwrap();
+            let token_user_b = get_input("Enter Token for User B: ").await.unwrap();
+            let user_a_address = get_input("Enter Address for User A: ").await.unwrap();
+            let user_b_address = get_input("Enter Address for User B: ").await.unwrap();
+        
+            (AuthUser{address: user_a_address, token: token_user_a}, AuthUser{address: user_b_address, token: token_user_b})
+        },
+    }
+}
+
+async fn extract_user(user: &serde_json::Value, user_id: &str) -> AuthUser {
+    let address =  match user["address"].as_str() {
+        Some(address) => address.to_string(),
+        None => {
+            let message = format!("Enter address for User {}: ", user_id);
+            get_input(&message).await.unwrap()
+        }
+    };
+    let token =  match user["token"].as_str() {
+        Some(token) => token.to_string(),
+        None => {
+            let message = format!("Enter token for User {}: ", user_id);
+            get_input(&message).await.unwrap()
+        }
+    };
+    AuthUser{address, token}
 }
